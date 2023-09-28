a7bb04c98ea53b7e379b3831ac169a674d996d2d
Loay Al-Said <l.programming555@gmail.com>
2023-09-27 19:28:31 -0700

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a square class"""
+
+
+class Square:
+    """Create a Square class and initiate the instase with a variable"""
+    def __init__(self, size):
+        """Init function, initialize object with size attribute"""
+        self.__size = size
