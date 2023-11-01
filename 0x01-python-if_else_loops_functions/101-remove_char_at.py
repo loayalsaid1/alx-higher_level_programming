d1b262d2e0dc8f70d704004909b7c32d769f9a96
loayalsaid1 <l.programming555@gmail.com>
2023-10-31 17:22:03 -0700

    Add 101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def detect_outrange_index(str, n):
+    if (n < 0) or (n >= len(str)):
+        return True
+
+
+def remove_char_at(str, n):
+    length = len(str)
+    if detect_outrange_index(str, n):
+        return (str)
+
+    new_cpy = ""
+    index = 0
+    for index in range(n):
+        new_cpy += str[index]
+
+    if index == 0:
+        index = 1
+    else:
+        index += 2
+    for index in range(index, length):
+        new_cpy += str[index]
+
+    return (new_cpy)
