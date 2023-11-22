7e2b92d433935fc9d862ff6ab2bcd51490c3c4e3
loayalsaid1 <l.programming555@gmail.com>
2023-11-22 11:42:33 -0800

    101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initialize a new square instance.
+
+        Args:
+            size: The size of the squar
+            position: Positionof the square
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """return the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """set the value of size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def position(self):
+        """Get position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """set position"""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """get the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """print the square with # symbols"""
+        if self.size == 0:
+            print()
+            return
+        for i in range(self.position[1]):
+            print()
+        for row in range(self.size):
+            [print(" ", end="") for j in range(self.position[0])]
+            for colomn in range(self.size):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """return the same as my_print output without last
+        newline as print will do it for me"""
+        square = ""
+        if self.size == 0:
+            return square
+        square += '\n' * self.position[1]
+        prefixes = ' ' * self.position[0]
+        line = prefixes + ('#' * self.size)
+        for i in range(self.size - 1):
+            square += line + '\n'
+        square += line
+        return square
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -5,3 +5,6 @@
 
 
 100-singly_linked_list.py -> -> simulate a singly linked list and have an sorted insert methosd as well
+
+
+101-square.py -> based on 6-square.py, but when printing it, do the same as my_print function
