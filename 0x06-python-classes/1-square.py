4324442fa15253f7172c576b552c8956ce14281e
loayalsaid1 <l.programming555@gmail.com>
2023-11-21 18:23:19 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        """ Initialize a new square instance.
+
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
