5b06a667f765cb9f9aed1e70c603712ac01a6e9e
loayalsaid1 <l.programming555@gmail.com>
2023-11-22 12:03:43 -0800

    102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """ Initialize a new square instance.
+
+        Args:
+            size: The size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """return the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """set the value of size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """get the area of the square"""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Define the == operation of 2 squares."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != operation of 2 squares."""
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        """Define the > operation of 2 squares."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= operation of 2 squares."""
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        """Define the < operation of 2 squares."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= operation of 2 squares."""
+        return self.area() <= other.area()
