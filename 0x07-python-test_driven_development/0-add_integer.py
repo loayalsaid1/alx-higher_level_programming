75a5541ee2a66daf1fdee770fab421c15e25d194
Loay Al-Said <l.programmig555@gmail.com>
2023-11-27 21:39:44 +0200

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+	add integer module
+"""
+
+
+def add_integer(a, b=98):
+	"""
+	add_integer: make integer addition
+	Only accept integers and if got floats, convert them
+	b is defaulted to 98
+	reutrn integer int(a) * int(b)
+	"""
+	if type(a) not in {int, float}:
+		raise TypeError("a must be an integer")
+	if type(b) not in {int, float}:
+		raise TypeError("b must be an integer")
+
+	return int(a) + int(b)
