f83c70a12c4f85c7aa20ac1b8e599ba9ebdb3d56
Loay Al-Said <l.programmig555@gmail.com>
2023-11-28 09:46:55 +0200

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to represent a rectangle and operations on it"""
+
+
+class Rectangle:
+    """
+    A class to represent a rectangle and operations one it
+    """
+    pass
