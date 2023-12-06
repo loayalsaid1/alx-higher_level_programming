c6be26fdb09a8fc3dcbe65845c389bb6b848f876
Loay Al-Said <l.programmig555@gmail.com>
2023-12-06 08:48:44 +0200

    Fix TDD task3 tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,4 +11,10 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is", first_name, last_name)
+    print("My name is", end='')
+    if first_name and last_name:
+        print(f" {first_name} {last_name}")
+    elif first_name:
+        print(f" {first_name} ")
+    else:
+        print()
