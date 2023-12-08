ea8eef524edf033e0ed5758e23099b3bc5220e0e
Loay Al-Said <l.programming555@gmail.com>
2023-12-08 16:53:32 +0200

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""SIMPLY, ISINSTANCE"""
+"""SIMPLY, check for direct childhood"""
 
 
 def is_same_class(obj, a_class):
-    """CHECK FOR INSTANCIATION OF AN OBJECT"""
+    """CHECK FOR direct childhood OF AN OBJECT"""
     return type(obj) == a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""SIMPLY, CHECK FOR TYPE OF OBJ BEAING SUBCLASS OF A CLASS"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ IS TYPE OF OBJECT, A SUBCLASS FROM <a_class> """
+    return isinstance(obj, a_class)
