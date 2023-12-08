87f70f9ded02b9849db8d2700063d21b3252e57b
Loay Al-Said <l.programming555@gmail.com>
2023-12-08 18:58:12 +0200

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""I don't know, yet 2"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+    def area(self):
+        """I don't know, yet 2"""
+        raise Exception("area() is not implemented")
