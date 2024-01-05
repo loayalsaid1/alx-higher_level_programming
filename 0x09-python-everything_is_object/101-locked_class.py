829a767393ab66211f83858481b0eb1da7c27230
Loay Al-Said <l.programming555@gmail.com>
2024-01-06 00:53:58 +0200

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A locked class mudule"""
+
+
+class LockedClass:
+    """A locked class
+    so you can't add any attibutes except first_name"""
+    def __setattr__(self, __name, __value):
+        if __name != "first_name":
+            raise AttributeError("'{}' object has no attribute {}".format(
+                self.__class__.__name__, __name
+            ))
+        else:
+            super().__setattr__(__name, __value)
