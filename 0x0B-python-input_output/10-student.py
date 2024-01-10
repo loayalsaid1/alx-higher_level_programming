5c8268123f48c797ca876745a48e612117285c50
Loay Al-Said <l.programming555@gmail.com>
2024-01-10 23:32:49 +0200

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Define a student class"""
+
+
+class Student:
+    """A student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize the instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation
+            of all the attributes of an <self>.
+            ----
+            If <attrs> are list of strings, Get only the names in that list.
+
+            Args:
+                self: The instance.
+                attrs: List of allowed names
+
+            Return: Dictionary of attributes based on above conditions.
+        """
+        condition = type(attrs) is list and\
+            any(type(attr) is not str for attr in attrs)
+        if attrs is None or condition:
+            return self.__dict__
+
+        dic = {}
+        for attr in attrs:
+            if attr in self.__dict__:
+                dic[attr] = self.__dict__[attr]
+        return dic
