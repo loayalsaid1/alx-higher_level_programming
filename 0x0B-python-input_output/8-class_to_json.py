e8b0215f33c315080084327527f51c690965de16
Loay Al-Said <l.programming555@gmail.com>
2024-01-10 22:01:00 +0200

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Return a dictionary representation of all the attributes of an instance"""
+
+
+def class_to_json(obj):
+    """Return a dictionary representation
+        of all the attributes of an <obj>.
+
+        Args:
+            obj: The instance
+
+        Return: YOU STILL DIDN'T GET IT?! !!!!!!!!!!!!
+    """
+    return obj.__dict__
