a76b0f4c907b727476465c9d865aaee4c3fd2016
Loay Al-Said <l.programming555@gmail.com>
2024-02-16 23:37:06 +0200

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Get all the titles with their genres in the database
+SELECT
+    tv_shows.title AS title,
+    tv_genres.name AS name
+FROM
+    tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title, name;
