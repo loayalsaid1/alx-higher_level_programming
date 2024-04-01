95c14242899f9a2ee7119c07c9e29236fade73d9
Loay Al-Said <l.programming555@gmail.com>
2024-04-02 00:39:05 +0200

    test

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Get the header of a url request responser"""
-from urllib.request import urlopen
-from sys import argv
-
-
-with urlopen(argv[1]) as response:
-    print(response.headers['X-Request-Id'])
