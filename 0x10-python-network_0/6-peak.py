d1c5ecd6c3b59a0f3738b594ad4214bc04b89323
Loay Al-Said <l.programming555@gmail.com>
2024-04-01 13:08:56 +0200

    6-peak.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,5 +4,22 @@
 
 def find_peak(l):
 	"""Find the peak of a list"""
-	l = l.sort()
-	return l[-1]
+	if not l:
+		return None
+	found = False
+	length = len(l)
+	if length == 1:
+		return l[0]
+	elif length == 2:
+		return max(l)
+	else:
+		for idx in range(1, length-1):
+			if l[idx-1] <= l[idx] >= l[idx+1]:
+				found = True
+				break
+		if found:
+			return l[idx]
+		else:
+			if l[0] > l[1]:
+				return l[0]
+		
