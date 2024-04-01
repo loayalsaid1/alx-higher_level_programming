238e2809cd819ff01a235ad9dfef59c78533ea3e
Loay Al-Said <l.programming555@gmail.com>
2024-04-01 13:24:40 +0200

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,24 +2,35 @@
 """Find a peak of a list"""
 
 
-def find_peak(l):
-	"""Find the peak of a list"""
-	if not l:
-		return None
-	found = False
-	length = len(l)
-	if length == 1:
-		return l[0]
-	elif length == 2:
-		return max(l)
-	else:
-		for idx in range(1, length-1):
-			if l[idx-1] <= l[idx] >= l[idx+1]:
-				found = True
-				break
-		if found:
-			return l[idx]
-		else:
-			if l[0] > l[1]:
-				return l[0]
-		
+def find_peak(ints):
+    """Find the peak of a list"""
+    if not ints:
+        return None
+    # found = False
+    # length = len(ints)
+    # if length == 1:
+    #     return ints[0]
+    # elif length == 2:
+    #     return max(ints)
+    # else:
+    #     for idx in range(1, length-1):
+    #         if ints[idx-1] <= ints[idx] >= ints[idx+1]:
+    #             found = True
+    #             break
+    #     if found:
+    #         return ints[idx]
+    #     else:
+    #         return max([ints[0], ints[-1]])
+
+    try:
+        if ints[0] > ints[1]:
+            return ints[0]
+        idx = 1
+        while (True):
+            if ints[idx-1] < ints[idx] > ints[idx+1]:
+                return ints[idx]
+            idx += 1
+    except IndexError:
+        return max(ints[0], ints[-1])
+    else:
+        return ints[0]
