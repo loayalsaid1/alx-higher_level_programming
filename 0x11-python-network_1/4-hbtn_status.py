d38a19184aff85d3268eab0a090de842a66a040d
Loay Al-Said <l.programming555@gmail.com>
2024-04-02 03:28:02 +0200

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetch a url vie the urllib package"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    result = response.text
+    print("Body response:")
+    print(f"\t- type: {type(result)}")
+    print(f"\t- content: {result}")
