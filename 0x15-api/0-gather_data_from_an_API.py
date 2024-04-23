d6a9f15681992a86d34f4d6113a5b162aad2145b
Loay Al-Said <l.programming555@gmail.com>
2024-04-23 04:40:59 +0200

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Use an api"""
+import requests
+import json
+import sys
+try:
+    user_id = sys.argv[1]
+except Exception:
+    exit()
+
+user = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+user = json.loads(user.text)
+
+tasks = requests.get(
+    f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
+tasks = json.loads(tasks.text)
+
+tasks_number = 0
+completed_tasks = 0
+titles = []
+
+for task in tasks:
+    tasks_number += 1
+    if task['completed']:
+        completed_tasks += 1
+        titles.append(task['title'])
+
+print(f"Employee {user['name']}",
+      f"is done with tasks({completed_tasks}/{tasks_number}):")
+for title in titles:
+    print(f"\t{title}")
