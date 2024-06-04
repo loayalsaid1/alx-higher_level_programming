991dd2d3a7f92c961d8d8d3f24085168a78d469d
Loay Al-Said <l.programming555@gmail.com>
2024-06-05 02:54:35 +0300

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,32 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-const argv = require('process').argv;
 
-if (argv.length !== 3) {
-  console.error('Usage: ./3-starwars_title.js <movie_id>');
-  process.exit(1);
+const filmID = process.argv[2];
+if (!filmID) {
+  process.exit();
 }
 
-const movieId = argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${filmID}`;
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Request failed with status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  try {
-    const movie = JSON.parse(body);
-    console.log(movie.title);
-  } catch (parseError) {
-    console.error('Error parsing response body:', parseError);
-    process.exit(1);
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const jsonResponse = JSON.parse(body);
+    console.log(jsonResponse.title);
   }
 });
