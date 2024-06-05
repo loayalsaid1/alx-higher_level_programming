939d9f7e89f9de914a252d4c8a795df5dbebf949
Loay Al-Said <l.programming555@gmail.com>
2024-06-05 05:33:08 +0300

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,22 @@
 
 const request = require('request');
 
-const api = 'https://swapi-api.alx-tools.com/api/people/18';
+const api = process.argv[2];
+const id = 18;
+let count = 0;
+
 request(api, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    const films = JSON.parse(body).films;
-
-    console.log(films.length);
+    const films = JSON.parse(body).results;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j].endsWith(`${id}/`)) {
+          count = count + 1;
+        }
+      }
+    }
+    console.log(count);
   }
 });
