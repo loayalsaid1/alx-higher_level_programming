84062d1230ba853e5c91cbb17945b1fc51e01ed6
Loay Al-Said <l.programming555@gmail.com>
2024-06-05 21:42:45 +0300

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,18 +7,19 @@ const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 
 let characters = [];
 
-request(url, (err, res, body) => {
-	if (!err) {
-		const film = JSON.parse(body);
-		characters = film.characters;
-		for (let i = 0; i < characters.length; i++) {
-			request(characters[i], (err, res, body) => {
-				if (!err) {
+async function printNamesInOrder (characters) {
+  for (const character of characters) {
+    let person = await fetch(character);
+    person = await person.json();
 
-					const person = JSON.parse(body);
-					console.log(person.name);
-				}
-			})
-		}
-	}
-})
+    console.log(person.name);
+  }
+}
+
+request(url, (err, res, body) => {
+  if (!err) {
+    const film = JSON.parse(body);
+    characters = film.characters;
+    printNamesInOrder(characters);
+  }
+});
